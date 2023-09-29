8abe8d562e778f3d85fe8a93091f3e8e9bce4c9b
David Kovlak <kovlakdavid4@gmail.com>
2023-09-29 13:05:33 -0700

    Done

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstname = "Miah";
+let lastname;
+var age;
 
-// Exercise 2
+lastname = "Harper";
+age = 20;
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hadRead = true;
+
+console.log(`Meet ${firstname} ${lastname}, who is ${age} years old.`);
+console.log(`They love programming in ${language} which was created in ${createdYear}.`);
+console.log(`They are currently reading "${title}" by ${author}.`);
+console.log(`It's a great book, but they wish it was shorter than its ${pageCount} pages!`);
